dfe79b60aaf38549a92ab71ff5e510864ef82f14
Karol <libakary@users.noreply.github.com>
2023-04-04 19:00:34 +0300

    Create function in Services and Interface

--- TARge21House/TARge21House.ApplicationServices/Services/HousesServices.cs
+++ TARge21House/TARge21House.ApplicationServices/Services/HousesServices.cs
@@ -1,4 +1,6 @@
-﻿using TARge21House.Core.ServiceInterface;
+﻿using TARge21House.Core.Domain;
+using TARge21House.Core.Dto;
+using TARge21House.Core.ServiceInterface;
 using TARge21House.Data;
 
 namespace TARge21House.ApplicationServices.Services
@@ -17,6 +19,28 @@ namespace TARge21House.ApplicationServices.Services
 		}
 
 		//create
+		public async Task<House> Create(HouseDto dto)
+		{
+			House house = new House();
+
+			house.Id = Guid.NewGuid();
+			house.MainColor = dto.MainColor;
+			house.RoofColor = dto.RoofColor;
+			house.Stories = dto.Stories;
+			house.Bedrooms = dto.Bedrooms;
+			house.Bathrooms = dto.Bathrooms;
+			house.RentPrice = dto.RentPrice;
+			house.PurchasePrice = dto.PurchasePrice;
+			house.BuiltDate = dto.BuiltDate;
+
+			house.CreatedAt = DateTime.Now;
+			house.ModifiedAt = DateTime.Now;
+
+			await _context.Houses.AddAsync(house);
+			await _context.SaveChangesAsync();
+
+			return house;
+		}
 		//update
 		//delete
 		//get async
--- TARge21House/TARge21House.Core/ServiceInterface/IHousesServices.cs
+++ TARge21House/TARge21House.Core/ServiceInterface/IHousesServices.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TARge21House.Core.Domain;
+using TARge21House.Core.Dto;
 
 namespace TARge21House.Core.ServiceInterface
 {
 	public interface IHousesServices
 	{
+		Task<House> Create(HouseDto dto);
+		Task<House> GetAsync(Guid id);
 	}
 }
